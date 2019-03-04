d2988d4c00b1c4fed189e5a466c2ddc6a325369d
Matthias Haegeman <haegeman.matthias@gmail.com>
2019-03-04 07:09:21 -0800

    - Nothing

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,5 @@
 class Flat < ApplicationRecord
+  has_many :reviews
+  has_many :bookings
+  has_many :flat_amenities
 end
--- app/models/flat_amenditie.rb
+++
@@ -1,4 +0,0 @@
-class FlatAmenditie < ApplicationRecord
-  belongs_to :flat
-  belongs_to :amenditie
-end
