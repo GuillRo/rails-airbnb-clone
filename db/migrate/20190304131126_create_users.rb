7c9c18fb8e3adc64b29b9466e4d8597b48f6f25f
Maxime van der Rest <maxime.vdr@gmail.com>
2019-03-04 17:00:18 +0100

    it is not the seeb but an update of the User model

--- db/migrate/20190304131126_create_users.rb
+++ db/migrate/20190304131126_create_users.rb
@@ -1,12 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
-      t.string :first_name
-      t.string :las_name
-      t.string :adress
-      t.string :email
 
-      t.timestamps
-    end
   end
 end
