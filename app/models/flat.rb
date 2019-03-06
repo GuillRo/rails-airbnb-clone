4622d75407d2f5c56c8bcf150e19f44af92183c0
Matthias Haegeman <haegeman.matthias@gmail.com>
2019-03-06 03:18:40 -0800

    Geocode back-end improvment

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -4,4 +4,6 @@ class Flat < ApplicationRecord
   has_many :flat_amenities
   has_many :amenities, through: :flat_amenities
   mount_uploader :photo, PhotoUploader
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
