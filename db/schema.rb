2c53ef9f3f89a3a70fc45fda7eef4fc0e5576ab4
Guillaume Rossi <guillaum.ro@gmail.com>
2019-03-06 12:14:15 +0100

    tiring

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_094606) do
+ActiveRecord::Schema.define(version: 2019_03_06_102319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 2019_03_06_094606) do
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "adress"
+    t.string "address"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
