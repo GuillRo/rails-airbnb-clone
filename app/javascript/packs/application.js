befa772fb5012be22aeeedd54d5a348f325b58d7
Guillaume Rossi <guillaum.ro@gmail.com>
2019-03-08 15:44:53 +0100

    bugfix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 
 import { initMapbox } from '../plugins/init_mapbox';
 import {loadDynamicBannerText} from "../components/banner";
+loadDynamicBannerText()
 initMapbox();
 
 
