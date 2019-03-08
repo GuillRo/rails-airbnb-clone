35c170fe6297f4b3355e2c7d5a0ef2cafee53bf4
Marie Hargitt <mhargitt@me.com>
2019-03-08 15:29:29 +0100

    homepage updates, typewriter, plus innerpage mods

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,9 @@ initUpdateNavbarOnScroll();
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
-
+import {loadDynamicBannerText} from "../components/banner";
 initMapbox();
-
+loadDynamicBannerText()
 
 import { algoliaSearch } from '../components/algolia';
 algoliaSearch();
