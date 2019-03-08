cb3f5aafc78525a4c3a318176111e25ce2ddaeb0
Matthias Haegeman <haegeman.matthias@gmail.com>
2019-03-08 04:39:54 -0800

    Calendar top

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_151929) do
+ActiveRecord::Schema.define(version: 2019_03_06_122734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "amenities", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "booked_days", force: :cascade do |t|
-    t.string "name"
-    t.datetime "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
